4d65814be9b74f45fe5d92796db47a33451a6660
Crowdbotics <team@crowdbotics.com>
2021-04-20 22:46:02 +0000

    Update models

--- backend/trippi_25804/wsgi.py
+++ backend/trippi_25804/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trippi_25804.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trippi_25804.settings")
 
 application = get_wsgi_application()
